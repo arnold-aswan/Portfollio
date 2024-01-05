7510d568d6c9c4cfa97fc436fd09d5860db01374
arnold-aswan <fredyaswan@gmail.com>
2024-01-05 16:17:05 +0300

    added skills to constants file

---
+++ constants/index.js
@@ -0,0 +1,40 @@
+const skillStacks = [
+  {
+    name: "css",
+    imgUrl: "/assets/skills/css.png",
+  },
+  {
+    name: "github",
+    imgUrl: "/assets/skills/github1.png",
+  },
+  {
+    name: "html",
+    imgUrl: "/assets/skills/html.png",
+  },
+  {
+    name: "javascript",
+    imgUrl: "/assets/skills/javascript.png",
+  },
+  {
+    name: "nextjs",
+    imgUrl: "/assets/skills/nextjs.png",
+  },
+  {
+    name: "react",
+    imgUrl: "/assets/skills/react.png",
+  },
+  {
+    name: "tailwind",
+    imgUrl: "/assets/skills/tailwind.png",
+  },
+  {
+    name: "mongo",
+    imgUrl: "/assets/skills/mongo.png",
+  },
+  {
+    name: "node",
+    imgUrl: "/assets/skills/node.png",
+  },
+];
+
+export default skillStacks;
