ebd9b83c49671080013cabf8fc9d3e530be2240e
arnold-aswan <fredyaswan@gmail.com>
2024-01-12 16:56:27 +0300

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white py-3 flex items-center justify-center">
+      <p>&#169;Arnold Aswani. All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
