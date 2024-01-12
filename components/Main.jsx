15aa726d24da21824def51836903f176689c8ec5
arnold-aswan <fredyaswan@gmail.com>
2024-01-12 16:57:16 +0300

    added animation & removed social icons from main section

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,26 +1,8 @@
 "use client";
 import React from "react";
-
-import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
-import { FaLinkedinIn, FaGithub, FaWhatsapp } from "react-icons/fa";
-import { IoLogoWhatsapp } from "react-icons/io";
+import { TypeAnimation } from "react-type-animation";
 
 const Main = () => {
-  const launchWhatsapp = () => {
-    // Check if WhatApp installed, if yes open whatsapp else open whatsapp web
-    const phone = "+254708581781";
-
-    // if (navigator.userAgent.includes("WhatsApp")) {
-    //   // WhatsApp is installed
-    if (window.innerWidth < 600) {
-      // If the window width is less than 600 pixels, open the WhatsApp client
-      window.open(`whatsapp://send?phone=${phone}`);
-    } else {
-      // If the window width is 600 pixels or more, open WhatsApp Web
-      window.open(`https://web.whatsapp.com/send?phone=${phone}`, "_blank");
-    }
-  };
-
   return (
     <section id="home" className="w-full h-screen text-center">
       <div className="max-w-[1248px] w-full h-full p-2 mx-auto flex justify-center items-center ">
@@ -29,7 +11,20 @@ const Main = () => {
             let&#39;s build something together
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&#39;m <span className="text-purple ">Arnold</span>
+            Hi, I&#39;m{" "}
+            <span className="text-purple ">
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed once, initially
+                  "Arnold",
+                  1000,
+                  "Aswani",
+                  1000,
+                ]}
+                speed={40}
+                repeat={Infinity}
+              />
+            </span>
           </h1>
           <h1 className="py-2 text-gray-400">A Front-End Web Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto ">
@@ -38,29 +33,6 @@ const Main = () => {
             building responsive front-end web applications integrating back-end
             technologies.
           </p>
-
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4 ">
-            <a
-              href="https://www.linkedin.com/in/arnold-aswani-390187205/"
-              target="_blank"
-              className="main__social__icons">
-              <FaLinkedinIn />
-            </a>
-            <a
-              href="https://github.com/arnold-aswan/"
-              target="_blank"
-              className="main__social__icons">
-              <FaGithub />
-            </a>
-            <a
-              href="mailto:fredyaswan@gmail.com"
-              className="main__social__icons">
-              <AiOutlineMail />
-            </a>
-            <div onClick={launchWhatsapp} className="main__social__icons">
-              <IoLogoWhatsapp />
-            </div>
-          </div>
         </div>
       </div>
     </section>
