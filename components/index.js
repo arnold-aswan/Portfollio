a21283e91e3da3021aab3cd2078203e1d24d2d94
arnold-aswan <fredyaswan@gmail.com>
2024-01-13 15:40:43 +0300

    imported connect & footer components

--- components/index.js
+++ components/index.js
@@ -3,5 +3,7 @@ import About from "./About";
 import Skills from "./Skills";
 import Projects from "./Projects";
 import ProjectItem from "./ProjectItem";
+import Connect from "./Connect";
+import Footer from "./Footer";
 
-export { Main, About, Skills, Projects, ProjectItem };
+export { Main, About, Skills, Projects, ProjectItem, Connect, Footer };
