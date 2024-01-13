7ea3ddcf42b844486df57379e86cdcbb8925f4f9
arnold-aswan <fredyaswan@gmail.com>
2024-01-13 15:38:23 +0300

    custom styling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,14 +4,14 @@ import { projects } from "@/constants";
 
 const Projects = () => {
   return (
-    <section className="w-full ">
+    <section id="projects" className="w-full ">
       <div className="max-w-[1248px] mx-auto px-2 py-16 ">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+        <p className="text-xl tracking-widest uppercase text-purple">
           Projects
         </p>
         <h2 className="py-4 capitalize">What i&#39;ve built</h2>
 
-        <div className="grid md:grid-cols-2 gap-8 ">
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 md:gap-4 lg:gap-8 place-items-center">
           {projects.map((project) => (
             <ProjectItem key={project.name} projects={project} />
           ))}
