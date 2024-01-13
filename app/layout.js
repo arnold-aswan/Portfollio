32766c3d48f94c123a4b0b36f4c7da288574e6e2
arnold-aswan <fredyaswan@gmail.com>
2024-01-13 15:40:15 +0300

    removed default font

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Footer } from "@/components";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Arnold Aswani",
